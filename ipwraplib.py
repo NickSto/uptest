83a6d9d0b4b4abfc7c00ed061c3a62f95a5dd752
Nick <nmapsy@gmail.com>
2016-12-29 16:09:58 -0500

    ipwraplib.py: Update to upstream commit 17e0acd

--- ipwraplib.py
+++ ipwraplib.py
@@ -2,13 +2,9 @@
 from the OS like wifi SSIDs, MAC addresses, DNS queries, etc."""
 import os
 import re
-import uuid
 import socket
 import subprocess
 import distutils.spawn
-
-DEFAULT_ROUTE_REGEX = r'^default\s+via\s+((?:\d{1,3}\.){3}\d{1,3})\s+dev\s+(\S+)\s+\S+\s+\S+\s*$'
-ARP_A_REGEX = r'^\S+\s+\(((?:\d{1,3}\.){3}\d{1,3})\)\s+at\s+((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\s+\S+\s+on\s+\S+\s*$'
 
 
 def get_wifi_info():
@@ -76,10 +72,14 @@ def get_default_route():
     devnull.close()
   # Parse output.
   for line in output.splitlines():
-    match = re.search(DEFAULT_ROUTE_REGEX, line)
-    if match:
-      ip = match.group(1)
-      interface = match.group(2)
+    fields = line.rstrip('\r\n').split()
+    if len(fields) < 7:
+      continue
+    # Expect a line like:
+    #   default via 10.21.160.1 dev wlan0  proto static
+    if fields[0] == 'default' and fields[1] == 'via' and fields[3] == 'dev':
+      ip = fields[2]
+      interface = fields[4]
       break
   return (interface, ip)
 
@@ -105,54 +105,64 @@ def dig_ip(domain):
   return None
 
 
-def get_mac_from_ip(ip):
-  """Use 'arp -a' command to look up the MAC address of an IP on the LAN.
-  Returns None on error, or if the IP isn't found."""
-  mac = None
-  arp_cmd = 'arp'
-  if 'PATH' not in os.environ or not distutils.spawn.find_executable(arp_cmd):
-    arp_cmd = '/usr/sbin/arp'
-  devnull = open(os.devnull, 'w')
+def dns_query(domain):
+  """Use the socket module to do a DNS query.
+  Returns None on failure instead of raising an exception (like socket.gaierror)."""
+  #TODO: Looks like getaddrinfo() is the preferred way?
   try:
-    output = subprocess.check_output([arp_cmd, '-a'], stderr=devnull)
-  except (OSError, subprocess.CalledProcessError):
+    return socket.gethostbyname(domain)
+  except socket.error:
     return None
-  finally:
-    devnull.close()
-  for line in output.splitlines():
-    match = re.search(ARP_A_REGEX, line)
-    if match and match.group(1) == ip:
-      mac = match.group(2)
-      return mac.upper()
-  return None
 
 
-def get_mac():
-  """Get your own device's MAC address using uuid.getnode().
-  Returns the MAC formatted in standard hex with colons."""
-  # uuid.getnode() returns the MAC as an integer.
-  mac_hex = hex(uuid.getnode())
-  # [2:] removes leading '0x'.
-  mac_hex = mac_hex[2:]
-  # Fill in leading 0's, if needed.
-  mac_hex = ('0' * (13 - len(mac_hex))) + mac_hex
-  # Remove trailing 'L'.
-  mac_hex = mac_hex[:12]
-  # Build mac from characters in mac_hex, inserting colons.
-  mac = ''
-  for (i, char) in enumerate(mac_hex):
-    if i > 1 and i % 2 == 0:
-      mac += ':'
-    mac += char
-  return mac
+def get_arp_table(proc_path='/proc/net/arp'):
+  """Get ARP table data from the /proc/net/arp pseudo-file.
+  Returns a dict mapping IP addresses to ARP table entries. Each entry is a dict
+  mapping field names to values. Fields: ip (str), hwtype (int), flags (int), mac
+  (str), mask (str), interface (str)."""
+  table = {}
+  header = True
+  with open(proc_path) as arp_table:
+    for line in arp_table:
+      # Skip the header.
+      if header:
+        header = False
+        continue
+      # Assume the file is whitespace-delimited.
+      fields = line.rstrip('\r\n').split()
+      try:
+        ip, hwtype, flags, mac, mask, interface = fields
+      except ValueError:
+        continue
+      try:
+        mac = mac.upper()
+        hwtype = int(hwtype, 16)
+        flags = int(flags, 16)
+      except ValueError:
+        continue
+      table[ip] = {'ip':ip, 'hwtype':hwtype, 'flags':flags, 'mac':mac, 'mask':mask,
+                   'interface':interface}
+  return table
+
+
+def get_mac_from_ip(ip):
+  """Look up the MAC address of an IP on the LAN, using the /proc/net/arp pseudo-file.
+  Returns None if the IP isn't found."""
+  arp_table = get_arp_table()
+  if ip in arp_table:
+    return arp_table[ip]['mac']
+  else:
+    return None
 
 
 def get_ip():
-  """Get this machine's IP address.
+  """Get this machine's local IP address.
   Should return the actual one used to connect to public IP's, if multiple
   interfaces are being used."""
   #TODO: Use get_default_route() to determine correct interface, and directly
   #      query its IP instead of kludge of making a dummy connection.
+  #      In the end, this is fundamentally not 100% correct, because packets to different public
+  #      IP's can be routed through different interfaces, depending on the local routing rules.
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   sock.connect(('8.8.8.8', 53))
   ip = sock.getsockname()[0]
