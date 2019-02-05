0d609cb098d149e94132637b7045703bd4c04f46
Nick <nmapsy@gmail.com>
2019-02-04 17:10:48 -0800

    marco/polo.py: Add (half of) feature to disguise as DNS query. - Finished encoding query as DNS. - Haven't yet encoded response as DNS.

--- marco.py
+++ marco.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import binascii
 import hashlib
 import logging
 import random
@@ -7,20 +8,36 @@ import socket
 import string
 import sys
 import time
+import urllib.parse
 assert sys.version_info.major >= 3, 'Python 3 required'
 
+DEFAULT_PORT = 35353
 HASH_CONST = b'Bust those caches!'
+DNS_HEADER = binascii.unhexlify(
+  '0100'  # flags (standard query)
+  '0001'  # 1 question
+  '0000'  # 0 answers
+  '0000'  # 0 authority records
+  '0000'  # 0 additional records
+)
+DNS_FOOTER = binascii.unhexlify(
+  '00'    # null-terminate the query field
+  '0001'  # query type: A record
+  '0001'  # query class: Internet
+)
 DESCRIPTION = """"""
 
 
 def make_argparser():
-  parser = argparse.ArgumentParser(description=DESCRIPTION)
+  parser = argparse.ArgumentParser(description=DESCRIPTION, add_help=False)
   parser.add_argument('message', nargs='?', default=get_rand_string(12),
     help='Data to send. If not given, this will send a random string.')
-  parser.add_argument('-i', '--ip', default='127.0.0.1',
-    help='Destination IP address. Default: %(default)s')
-  parser.add_argument('-p', '--port', type=int, default=35353,
-    help='Port to send to. Default: %(default)s')
+  parser.add_argument('-d', '--dns', action='store_true',
+    help='Disguise the data as a DNS query. Changes the default port to 53.')
+  parser.add_argument('-h', '--host', default='127.0.0.1',
+    help='Destination host. Give an IP address or domain name. Default: %(default)s')
+  parser.add_argument('-p', '--port', type=int,
+    help='Port to send to. Default: {}'.format(DEFAULT_PORT))
   parser.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
   volume = parser.add_mutually_exclusive_group()
@@ -28,6 +45,8 @@ def make_argparser():
     default=logging.WARNING)
   volume.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
   volume.add_argument('-D', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
+  parser.add_argument('-H', '--help', action='help',
+    help='Show this help message and exit.')
   return parser
 
 
@@ -38,12 +57,23 @@ def main(argv):
 
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
+  if args.port:
+    port = args.port
+  elif args.dns:
+    port = 53
+  else:
+    port = DEFAULT_PORT
+
   message_bytes = bytes(args.message, 'utf8')
+  if args.dns:
+    message_encoded = encode_dns_query(args.message)
+  else:
+    message_encoded = message_bytes
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   try:
     start = time.time()
-    sock.sendto(message_bytes, (args.ip, args.port))
+    sock.sendto(message_encoded, (args.host, port))
     data, (addr, port) = sock.recvfrom(1024)
     elapsed = time.time() - start
     digest = get_hash(HASH_CONST+message_bytes)
@@ -54,6 +84,34 @@ def main(argv):
       print('Response differs from expected: {}'.format(bytes_to_hex(digest)))
   finally:
     sock.close()
+
+
+def get_random_bytes(length):
+  integers = [random.randint(0, 255) for i in range(length)]
+  return bytes(integers)
+
+
+def encode_dns_query(message, txn_id=None):
+  if txn_id is None:
+    txn_id = get_random_bytes(2)
+  elif len(txn_id) != 2:
+    raise ValueError('Transaction ID {} is not 2 bytes long.'.format(txn_id))
+  query = txn_id + DNS_HEADER
+  query += encode_dns_message(message)
+  query += DNS_FOOTER
+  return query
+
+
+def encode_dns_message(message):
+  message_encoded = b''
+  message_quoted = urllib.parse.quote_plus(message)
+  for field in message_quoted.split('.'):
+    length = len(field)
+    if length >= 256:
+      raise ValueError('Message contains a dot-delimited, url-encoded field longer than '
+                       '255 characters: {}'.format(field))
+    message_encoded += length.to_bytes(1, byteorder='big') + bytes(field, 'utf8')
+  return message_encoded
 
 
 def get_rand_string(length):
--- polo.py
+++ polo.py
@@ -4,8 +4,11 @@ import hashlib
 import logging
 import socket
 import sys
+import urllib.parse
+import marco
 assert sys.version_info.major >= 3, 'Python 3 required'
 
+DEFAULT_PORT = 35353
 HASH_CONST = b'Bust those caches!'
 DESCRIPTION = """Reply to pings from upmonitor clients with expected responses.
 This server listens to UDP packets on the given port, and replies with a response derived from the
@@ -19,8 +22,10 @@ def make_argparser():
   parser = argparse.ArgumentParser(description=DESCRIPTION)
   parser.add_argument('ip', nargs='?', default='127.0.0.1',
     help='Listen IP address. Default: %(default)s')
-  parser.add_argument('-p', '--port', type=int, default=35353,
-    help='Port to listen on. Default: %(default)s')
+  parser.add_argument('-d', '--dns', action='store_true',
+    help='Expect queries in DNS format. Changes the default port to 53.')
+  parser.add_argument('-p', '--port', type=int,
+    help='Port to listen on. Default: {}'.format(DEFAULT_PORT))
   parser.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
   volume = parser.add_mutually_exclusive_group()
@@ -38,27 +43,64 @@ def main(argv):
 
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
+  if args.port:
+    port = args.port
+  elif args.dns:
+    port = 53
+  else:
+    port = DEFAULT_PORT
+
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind((args.ip, args.port))
+  sock.bind((args.ip, port))
   if logging.getLogger().getEffectiveLevel() < logging.CRITICAL:
     print('Listening on {} port {}..'.format(args.ip, args.port), file=sys.stderr)
 
   try:
     while True:
-      listen(sock)
+      listen(sock, dns=args.dns)
   except KeyboardInterrupt:
     logging.info('Interrupted by user.')
   finally:
     sock.close()
 
 
-def listen(sock, hash_const=HASH_CONST):
+def listen(sock, hash_const=HASH_CONST, dns=False):
   while True:
-    data, (addr, port) = sock.recvfrom(1024)
-    logging.info('Received from {} port {}: {!r}'.format(addr, port, data))
-    digest = get_hash(hash_const+data)
+    contents, (addr, port) = sock.recvfrom(1024)
+    logging.info('Received from {} port {}: {!r}'.format(addr, port, contents))
+    if dns:
+      txn_id, message = decode_dns_query(contents)
+      message_bytes = bytes(message, 'utf8')
+    else:
+      message_bytes = contents
+    digest = get_hash(hash_const+message_bytes)
     sock.sendto(digest, (addr, port))
     logging.info('Replied with hash {}'.format(bytes_to_hex(digest)))
+
+
+def decode_dns_query(query):
+  txn_id = query[:2]
+  header = query[2:2+len(marco.DNS_HEADER)]
+  message_encoded = query[2+len(marco.DNS_HEADER):-len(marco.DNS_FOOTER)]
+  footer = query[-len(marco.DNS_FOOTER):]
+  if header != marco.DNS_HEADER:
+    raise ValueError('Malformed header: {}'.format(header))
+  if footer != marco.DNS_FOOTER:
+    raise ValueError('Malformed footer: {}'.format(footer))
+  message = decode_dns_message(message_encoded)
+  return txn_id, message
+
+
+def decode_dns_message(message_encoded):
+  fields = []
+  message_remaining = message_encoded
+  while message_remaining:
+    field_len = message_remaining[0]
+    field_bytes = message_remaining[1:1+field_len]
+    fields.append(str(field_bytes, 'utf8'))
+    message_remaining = message_remaining[1+field_len:]
+  message_quoted = '.'.join(fields)
+  return urllib.parse.unquote_plus(message_quoted)
 
 
 def get_hash(data, algorithm='sha256'):
