05ce047719330abe649ab31e01ebe984e761c30c
Nick <nmapsy@gmail.com>
2019-02-06 22:10:48 -0800

    marco/polo.py: Add a new TCP protocol. - And rearrange everything to accommodate both. - Help text isn't updated yet, though.

--- marco.py
+++ marco.py
@@ -11,6 +11,13 @@ import fauxdns
 import polo
 assert sys.version_info.major >= 3, 'Python 3 required'
 
+#TODO: Change of strategy. Seems it might be common for access points to block DNS requests to
+#      arbitrary DNS servers (instead of the DHCP one).
+#      So instead, maybe go back to TCP, but make the connection manually. Time how long the
+#      sock.connect() method takes. In testing, this seems accurate. Yes, it includes the time the
+#      system takes to send out the final ACK, but that's a local operation that's very fast.
+#      To address caching, use the TCP connection to send the challenge to polo.py.
+
 FAILURE_EXIT_CODE = 7
 CACHING_EXIT_CODE = 13
 INTERCEPTION_EXIT_CODE = 17
@@ -25,13 +32,21 @@ def make_argparser():
   parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG, add_help=False)
   parser.add_argument('message', nargs='?', default=get_rand_string(12)+'.com',
     help='Data to send. If not given, this will send a random string.')
-  parser.add_argument('-d', '--dns', action='store_true',
-    help='Disguise the data as a DNS query. Changes the default port to 53.')
+  parser.add_argument('-u', '--udp', dest='protocol', action='store_const', const='udp', default='udp',
+    help='Use raw UDP as the protocol (the default).')
+  parser.add_argument('-t', '--tcp', dest='protocol', action='store_const', const='tcp',
+    help='Use raw TCP as the protocol.')
+  parser.add_argument('-d', '--dns', dest='protocol', action='store_const', const='dns',
+    help='Use DNS as the protocol. The data will be disguised as a DNS query. Changes the default '
+         'port to 53.')
+  parser.add_argument('-w', '--http', dest='protocol', action='store_const', const='http',
+    help='Use HTTP as the protocol.')
   parser.add_argument('-h', '--host', default='127.0.0.1',
     help='Destination host. Give an IP address or domain name. Default: %(default)s')
   parser.add_argument('-p', '--port', type=int,
-    help='Port to send to. Default: {}'.format(polo.DEFAULT_PORT))
-  parser.add_argument('-t', '--tsv', action='store_const', dest='format', const='computer',
+    help='Port to send to. Default for UDP/DNS: {}. Default for TCP/HTTP: {}'
+         .format(polo.UDP_PORT, polo.TCP_PORT))
+  parser.add_argument('-c', '--tsv', action='store_const', dest='format', const='computer',
     default='human',
     help='Print results in computer-readable format. Currently it will just print the latency in '
          'milliseconds.')
@@ -54,23 +69,68 @@ def main(argv):
 
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
+  if args.protocol == 'udp':
+    transport = 'udp'
+    application = 'raw'
+  elif args.protocol == 'dns':
+    transport = 'udp'
+    application = 'dns'
+  elif args.protocol == 'tcp':
+    transport = 'tcp'
+    application = 'raw'
+  elif args.protocol == 'http':
+    transport = 'tcp'
+    application = 'http'
+
   if args.port:
     port = args.port
-  elif args.dns:
-    port = 53
-  else:
-    port = polo.DEFAULT_PORT
+  elif transport == 'udp':
+    port = polo.UDP_PORT
+  elif transport == 'tcp':
+    port = polo.TCP_PORT
 
   #TODO: Use getaddrinfo() to support IPv6.
   ip = socket.gethostbyname(args.host)
 
   message_bytes = bytes(args.message, 'utf8')
-  if args.dns:
-    txn_id = get_random_bytes(2)
-    message_encoded = fauxdns.encode_dns_query(args.message, txn_id)
-  else:
-    message_encoded = message_bytes
+  expected_digest = polo.get_hash(message_bytes)
 
+  if transport == 'udp':
+    try:
+      response_digest, stats = send_udp(ip, port, args.message, application)
+    except ValueError as error:
+      logging.warning('Warning: Malformed response. Probably a response from a real DNS server.\n'
+                      +str(error))
+      return INTERCEPTION_EXIT_CODE
+  elif transport == 'tcp':
+    response_digest, stats = send_tcp(ip, port, args.message, application)
+
+  if args.format == 'human':
+    if transport == 'udp':
+      print('Received response from {ip} port {port} in {elapsed:0.1f} ms.'
+            .format(**stats))
+    elif transport == 'tcp':
+      print('Connected to {} port {} in {elapsed:0.1f} ms.'
+            .format(ip, port, **stats))
+  else:
+    print('{elapsed:0.1f}'.format(**stats))
+
+  if response_digest == expected_digest:
+    if args.format == 'human':
+      print('Response is as expected: {}'.format(polo.bytes_to_hex(response_digest)))
+  else:
+    if args.format == 'human':
+      print('Response differs from expected: {}'.format(polo.bytes_to_hex(expected_digest)))
+      logging.info('Expected:                       {}'.format(polo.bytes_to_hex(response_digest)))
+    return CACHING_EXIT_CODE
+
+
+def send_udp(ip, port, message, application):
+  if application == 'dns':
+    txn_id = get_random_bytes(2)
+    message_encoded = fauxdns.encode_dns_query(message, txn_id)
+  else:
+    message_encoded = bytes(message, 'utf8')
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   try:
     start = timeit.default_timer()
@@ -80,35 +140,32 @@ def main(argv):
       logging.critical('Error: Could not connect to remote server.')
       return FAILURE_EXIT_CODE
     logging.info('Sent query; waiting on reply..')
-    response, (addr, port) = sock.recvfrom(1024)
+    response, (remote_ip, remote_port) = sock.recvfrom(polo.BUFFER_SIZE)
     elapsed = timeit.default_timer() - start
-    expected_digest = polo.get_hash(message_bytes)
-    if args.format == 'human':
-      print('Received response from {} port {} in {:0.1f} ms'.format(addr, port, elapsed*1000))
-    else:
-      print('{:0.1f}'.format(elapsed*1000))
-    if args.dns:
-      try:
-        response_txn_id, query, answer = fauxdns.split_dns_response(response)
-        response_digest = fauxdns.extract_dns_answer(answer)
-      except ValueError as error:
-        logging.warning('Warning: Malformed response. Probably a response from a real DNS server.\n'
-                        +str(error))
-        return INTERCEPTION_EXIT_CODE
-    else:
-      response_digest = response
+  finally:
+    sock.close()
+  if application == 'raw':
+    response_digest = response
+  else:
+    response_txn_id, query, answer = fauxdns.split_dns_response(response)
+    response_digest = fauxdns.extract_dns_answer(answer)
     if response_txn_id != txn_id:
       logging.error('Response transaction ID ({}) is different from query\'s ({}).'
                     .format(fauxdns.bytes_to_int(response_txn_id), fauxdns.bytes_to_int(txn_id)))
-    if response_digest == expected_digest:
-      if args.format == 'human':
-        print('Response is as expected: {}'.format(polo.bytes_to_hex(response_digest)))
-    else:
-      if args.format == 'human':
-        print('Response differs from expected: {}'.format(polo.bytes_to_hex(expected_digest)))
-      return CACHING_EXIT_CODE
-  finally:
-    sock.close()
+  return response_digest, {'elapsed':elapsed*1000, 'ip':remote_ip, 'port':remote_port}
+
+
+def send_tcp(ip, port, message, application):
+  if application == 'raw':
+    message_encoded = bytes(message+'\n', 'utf8')
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    start = timeit.default_timer()
+    sock.connect((ip, port))
+    end = timeit.default_timer()
+    elapsed = end - start
+    sock.sendall(message_encoded)
+    response = sock.recv(polo.BUFFER_SIZE)
+  return response, {'elapsed':elapsed*1000, 'ip':None, 'port':None}
 
 
 def get_random_bytes(length):
--- polo.py
+++ polo.py
@@ -9,7 +9,9 @@ import urllib.parse
 import fauxdns
 assert sys.version_info.major >= 3, 'Python 3 required'
 
-DEFAULT_PORT = 35353
+TCP_PORT = 18088
+UDP_PORT = 35353
+BUFFER_SIZE = 1024
 HASH_CONST = b'Bust those caches!'
 DESCRIPTION = """Reply to pings from upmonitor clients with expected responses.
 This server listens to UDP packets on the given port, and replies with a response derived from the
@@ -21,18 +23,27 @@ EPILOG = """Listening directly on port 53 is not recommended, since it requires 
 as root. If you'd like to receive queries on port 53, you should instead use iptables to redirect
 traffic from port 53 to the port this is listening on. If this is listening to {0}, then you can
 use: $ sudo iptables -t nat -I PREROUTING --src 0/0 -p udp --dport 53 -j REDIRECT --to-ports {0}
-""".format(DEFAULT_PORT)
+""".format(UDP_PORT)
+#TODO: Figure out how to use nginx to proxy connections to a certain host or url to a different
+#      port, allowing this to co-exist with a website on the same server!
 
 def make_argparser():
-  parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
+  parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG, add_help=False)
   parser.add_argument('ip', nargs='?', default='127.0.0.1',
     help='Listen IP address. Default: %(default)s')
-  parser.add_argument('-d', '--dns', action='store_true',
-    help='Expect queries in DNS format. The contents will be in the place where the domain name is '
+  parser.add_argument('-u', '--udp', dest='protocol', action='store_const', const='udp', default='udp',
+    help='Use raw UDP as the protocol (the default).')
+  parser.add_argument('-t', '--tcp', dest='protocol', action='store_const', const='tcp',
+    help='Use raw TCP as the protocol.')
+  parser.add_argument('-d', '--dns', dest='protocol', action='store_const', const='dns',
+    help='Use DNS as the protocol. The challenge will be in the place where the domain name is '
          'given in a DNS query. This script will then respond with a DNS response, encoding the '
          'hash where the IP address is normally given.')
-  parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT,
-    help='Port to listen on. Default: %(default)s')
+  parser.add_argument('-w', '--http', dest='protocol', action='store_const', const='http',
+    help='Use HTTP as the protocol.')
+  parser.add_argument('-p', '--port', type=int,
+    help='Port to listen on. Default for UDP/DNS: {}. Default for TCP/HTTP: {}'
+         .format(UDP_PORT, TCP_PORT))
   parser.add_argument('-l', '--log', type=argparse.FileType('w'), default=sys.stderr,
     help='Print log messages to this file instead of to stderr. Warning: Will overwrite the file.')
   volume = parser.add_mutually_exclusive_group()
@@ -40,6 +51,8 @@ def make_argparser():
     default=logging.WARNING)
   volume.add_argument('-v', '--verbose', dest='volume', action='store_const', const=logging.INFO)
   volume.add_argument('-D', '--debug', dest='volume', action='store_const', const=logging.DEBUG)
+  parser.add_argument('-H', '--help', action='help',
+    help='Show this help message and exit.')
   return parser
 
 
@@ -50,24 +63,50 @@ def main(argv):
 
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind((args.ip, args.port))
+  if args.port:
+    port = args.port
+  if args.protocol in ('udp', 'dns'):
+    transport = 'udp'
+    if not args.port:
+      port = UDP_PORT
+  elif args.protocol in ('tcp', 'http'):
+    transport = 'tcp'
+    if not args.port:
+      port = TCP_PORT
+
+  if args.protocol in ('udp', 'tcp'):
+    application = 'raw'
+  else:
+    application = args.protocol
+
+  if transport == 'udp':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  elif transport == 'tcp':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  sock.bind((args.ip, port))
+
   if logging.getLogger().getEffectiveLevel() < logging.CRITICAL:
-    print('Listening on {} port {}..'.format(args.ip, args.port), file=sys.stderr)
+    print('Listening on {} port {}..'.format(args.ip, port), file=sys.stderr)
 
   try:
     while True:
-      listen(sock, dns=args.dns)
+      if transport == 'udp':
+        listen_udp(sock, application)
+      elif transport == 'tcp':
+        listen_tcp(sock, application)
   except KeyboardInterrupt:
     logging.info('Interrupted by user.')
   finally:
     sock.close()
 
 
-def listen(sock, hash_const=HASH_CONST, dns=False):
+def listen_udp(sock, application, hash_const=HASH_CONST):
   while True:
-    contents, (addr, port) = sock.recvfrom(1024)
-    if dns:
+    contents, (ip, port) = sock.recvfrom(BUFFER_SIZE)
+    if application == 'raw':
+      message_bytes = contents
+    elif application == 'dns':
       try:
         txn_id, message_encoded = fauxdns.split_dns_query(contents)
         message = fauxdns.decode_dns_message(message_encoded)
@@ -75,20 +114,43 @@ def listen(sock, hash_const=HASH_CONST, dns=False):
         logging.error('Error: Problem parsing incoming query:\n'+str(error))
         continue
       message_bytes = bytes(message, 'utf8')
-    else:
-      message_bytes = contents
-    logging.info('Received from {} port {}: {!r}'.format(addr, port, message_bytes))
-    digest = get_hash(message_bytes, hash_const=HASH_CONST)
-    if dns:
+    logging.info('Received from {} port {}: {!r}'.format(ip, port, message_bytes))
+    digest = get_hash(message_bytes, hash_const=hash_const)
+    if application == 'raw':
+      response = digest
+    elif application == 'dns':
       try:
         response = fauxdns.encode_dns_response(txn_id, message_encoded, digest)
       except ValueError as error:
         logging.error('Error: Problem encoding response:\n'+str(error))
         continue
-    else:
-      response = digest
-    sock.sendto(response, (addr, port))
+    sock.sendto(response, (ip, port))
     logging.info('Replied with hash {}'.format(bytes_to_hex(digest)))
+
+
+def listen_tcp(sock, application, hash_const=HASH_CONST):
+  sock.listen()
+  connection, (ip, port) = sock.accept()
+  with connection:
+    contents = b''
+    while True:
+      logging.debug('Waiting to receive data..')
+      buf = connection.recv(BUFFER_SIZE)
+      logging.debug('Received {} bytes, ending in: {}'.format(len(buf), buf[-10:]))
+      if not buf:
+        break
+      elif buf.endswith(b'\n'):
+        contents += buf[:-1]
+        break
+      else:
+        contents += buf
+    message_bytes = contents
+    logging.info('Received from {} port {}: {!r}'.format(ip, port, message_bytes))
+    if application == 'raw':
+      digest = get_hash(message_bytes, hash_const=hash_const)
+    elif application == 'http':
+      raise NotImplementedError
+    connection.sendall(digest)
 
 
 def get_hash(data, hash_const=HASH_CONST, algorithm='sha256'):
