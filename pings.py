da8c45a2c679c14c618fbfd96df6bb062a4ddacc
Nick <nmapsy@gmail.com>
2019-02-08 02:07:12 -0800

    pings.py: Move all the methods of connection checking here. - Move it out of upmonitor.py.

---
+++ pings.py
@@ -0,0 +1,215 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+import os
+import re
+import timeit
+import socket
+import httplib
+import subprocess
+try:
+  import dns.resolver
+  import dns.exception
+except ImportError:
+  pass
+
+
+# These headers might take care of hotspot caches.
+HTTP_HEADERS = {'Cache-Control':'no-cache', 'Pragma':'no-cache'}
+
+
+def get_ping_version():
+  """Try to determine what version of ping is available.
+  Currently recognizes the BSD and iputils versions, returning "bsd" and
+  "iputils" for them, respectively. For all others, returns the first line of
+  the output of "ping -V". On error, returns None."""
+  devnull = open(os.devnull, 'w')
+  # Call command.
+  try:
+    output = subprocess.check_output(['ping', '-V'], stderr=devnull)
+    exit_status = 0
+  except subprocess.CalledProcessError as cpe:
+    output = cpe.output
+    exit_status = cpe.returncode
+  except OSError:
+    output = ''
+    exit_status = 1
+  finally:
+    devnull.close()
+  # BSD lacks -V option and errors out with exit status 64
+  if exit_status == 64 and output == '':
+    return 'bsd'
+  elif exit_status != 0:
+    return None
+  # Parse output
+  output_lines = output.splitlines()
+  if len(output_lines) == 0:
+    return None
+  if 'iputils' in output_lines[0]:
+    return 'iputils'
+  else:
+    return output_lines[0]
+
+
+def ping(server, method='ping', timeout=2, ping_ver=None):
+  """Ping "server", and return the ping time in milliseconds.
+  If the ping fails, returns 0.
+  If the method is "curl", the returned time is the "time_connect" variable of
+  curl's "-w" option (multiplied by 1000 to get ms). In practice the time is
+  very similar to a simple ping."""
+  devnull = open(os.devnull, 'w')
+  # Build command.
+  assert method in ['ping', 'curl'], 'Error: Invalid ping method'
+  if method == 'ping':
+    # Timeout depends on which version of ping. If it can't be determined, don't set timeout.
+    if ping_ver == 'iputils':
+      command = ['ping', '-n', '-c', '1', '-w', str(timeout), server]
+    elif ping_ver == 'bsd':
+      command = ['ping', '-n', '-c', '1', '-t', str(timeout), server]
+    else:
+      command = ['ping', '-n', '-c', '1', server]
+  elif method == 'curl':
+    command = ['curl', '-s', '--output', '/dev/null', '--write-out', r'%{time_connect}',
+               '--connect-timeout', str(timeout), server]
+  # Call command.
+  try:
+    output = subprocess.check_output(command, stderr=devnull)
+    exit_status = 0
+  except subprocess.CalledProcessError as cpe:
+    output = cpe.output
+    exit_status = cpe.returncode
+  except OSError:
+    output = ''
+    exit_status = 1
+  finally:
+    devnull.close()
+  # Parse output or return 0.0 on error.
+  if exit_status == 0:
+    if method == 'ping':
+      return parse_ping(output)
+    elif method == 'curl':
+      return parse_curl(output)
+  else:
+    return 0.0
+
+
+def parse_ping(ping_str):
+  """Parse out the ms of the ping from the output of `ping -n -c 1`"""
+  ping_pattern = r' bytes from .* time=([\d.]+) ?ms'
+  for line in ping_str.splitlines():
+    match = re.search(ping_pattern, line)
+    if match:
+      try:
+        return float(match.group(1))
+      except ValueError:
+        return 0.0
+  return 0.0
+
+
+def parse_curl(curl_str):
+  """Parse the output of curl into a number of milliseconds (float)."""
+  try:
+    return 1000*float(curl_str)
+  except ValueError:
+    return 0.0
+
+
+def ping_and_check(timeout=2, server='www.gstatic.com', path='/generate_204', status=204, body=''):
+  """"Ping" a server with an HTTP GET request, returning the latency and whether
+  the response appears to be intercepted (i.e. by a captive portal).
+  By default, uses http://www.gstatic.com/generate_204 and assumes interception
+  if the response code isn't 204 or the body isn't "". You can customize the
+  url and expected response with the respective parameters.
+  The latency is determined from the TCP handshake, so it should be a single
+  round trip.
+  Returns (float, bool): latency in milliseconds and whether the response looks
+  intercepted. If no connection can be established, returns (0.0, None). If an
+  error is encountered at any point, returns None for the second value."""
+  elapsed, response = ping_http(timeout=timeout, server=server, path=path, body=body)
+  if response is None:
+    return (0.0, None)
+  # Is the response as expected?
+  # If only an expected status is given (body is None), only that has to match.
+  # If a status and body is given, both have to match.
+  if response['status'] == status and (body is None or response['body'] == body):
+    expected = True
+  else:
+    expected = False
+  return (elapsed, not expected)
+
+
+def ping_http(timeout=2, server='www.gstatic.com', path='/generate_204', body=''):
+  # Do the DNS lookup outside the timed portion of the connection, where we only want to measure the
+  # TCP handshake, not any needed DNS lookup.
+  ip = dns_lookup(server, timeout=timeout)
+  # Create the connection object.
+  conex = httplib.HTTPConnection(ip, timeout=timeout)
+  # Open a connection to the server. connect() just establishes the TCP connection with a
+  # SYN, SYN/ACK, ACK handshake, returning after the final ACK is sent. This is essentially
+  # immediately after the SYN/ACK arrives, making it a good measure of a single round trip.
+  before = timeit.default_timer()
+  try:
+    conex.connect()
+  except (httplib.HTTPException, socket.error):
+    return (0.0, None)
+  after = timeit.default_timer()
+  elapsed = round(1000 * (after - before), 1)
+  # Make the HTTP request.
+  # We have to define the Host header explicitly to avoid it appearing as the IP address.
+  # Note: This won't work with HTTPS, since the host will be passed in the SNI.
+  headers = HTTP_HEADERS.copy()
+  headers['Host'] = server
+  try:
+    conex.request('GET', path, None, headers)
+  except (httplib.HTTPException, socket.error):
+    return (0.0, None)
+  # Read the HTTP response.
+  try:
+    response = conex.getresponse()
+  except (httplib.HTTPException, socket.error):
+    return (0.0, None)
+  # We have to pass back a dict of response values instead of the response itself because you can't
+  # read the response body after the connection is closed.
+  response_dict = {'status':response.status, 'body':response.read(len(body)+1)}
+  conex.close()
+  return (elapsed, response_dict)
+
+
+def dns_lookup(domain, timeout=2):
+  """Do a DNS lookup with a certain timeout, if possible."""
+  try:
+    # Try using dns.resolver, if it's installed.
+    return dns_lookup_dns(domain, timeout=timeout)
+  except NameError:
+    # Otherwise, fall back to socket.gethostbyname().
+    return dns_lookup_socket(domain)
+
+
+def dns_lookup_dns(domain, timeout=2):
+  """Use "dns" module to look up an IP address, returning in a specified amount of time.
+  Returns None on timeout or error."""
+  resolver = dns.resolver.Resolver()
+  resolver.timeout = timeout
+  resolver.lifetime = timeout
+  try:
+    results = resolver.query(domain)
+  except dns.exception.DNSException:
+    return None
+  try:
+    # Just pick the first IP address returned.
+    return str(results[0])
+  except IndexError:
+    return None
+
+
+def dns_lookup_socket(domain):
+  """Use socket.gethostbyname() to look up an IP address.
+  Returns None on error."""
+  #TODO: Set timeout using signal.alarm():
+  #      https://stackoverflow.com/questions/492519/timeout-on-a-python-function-call/494273#494273
+  try:
+    ip = socket.gethostbyname(domain)
+  except (socket.timeout, socket.gaierror):
+    return None
+  return ip
--- upmonitor.py
+++ upmonitor.py
@@ -13,31 +13,21 @@
 #      a wifi router with no internet connection), ping doesn't "obey" the -W timeout, getting stuck
 #      in a DNS lookup. Seems the only way to resolve this situation is to do the DNS yourself and
 #      give ping an actual ip address.
-#TODO: The Cache-Control/Pragma headers might be enough to take care of hotspot caches.
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
-import re
 import os
 import sys
 import copy
 import time
 import errno
-import timeit
 import signal
-import socket
-import httplib
 import numbers
 import argparse
-import subprocess
 import ConfigParser
 import ipwraplib
-try:
-  import dns.resolver
-  import dns.exception
-except ImportError:
-  pass
+import pings
 
 
 DATA_DIR_DEFAULT = '.local/share/nbsdata'
@@ -46,7 +36,6 @@ HISTORY_FILENAME = 'uphistory.txt'
 STATUS_FILENAME = 'upstatus.txt'
 CONFIG_FILENAME = 'upmonitor.cfg'
 SHUTDOWN_STATUS = 'OFFLINE'
-HTTP_HEADERS = {'Cache-Control':'no-cache', 'Pragma':'no-cache'}
 DETECTORS = {
   'google': {'server':'www.gstatic.com', 'path':'/generate_204', 'status':204, 'body':''},
   'mozilla': {'server':'detectportal.firefox.com', 'path':'/success.txt', 'status':200,
@@ -155,7 +144,8 @@ def main():
   sys.excepthook = invalidate_and_reraise
 
   # What version of ping?
-  ping_ver = get_ping_version()
+  if args.method == 'ping':
+    ping_ver = pings.get_ping_version()
 
   # Main loop.
   now = int(time.time())
@@ -202,9 +192,9 @@ def main():
     if args.method == 'httplib':
       server = DETECTOR_ALIASES.get(args.server, args.server)
       detector = DETECTORS[server]
-      (result, intercepted) = ping_and_check(timeout=args.timeout, **detector)
+      (result, intercepted) = pings.ping_and_check(timeout=args.timeout, **detector)
     else:
-      result = ping(args.server, method=args.method, timeout=args.timeout, ping_ver=ping_ver)
+      result = pings.ping(args.server, method=args.method, timeout=args.timeout, ping_ver=ping_ver)
       intercepted = None
     if result:
       if intercepted is True:
@@ -397,169 +387,6 @@ def prune_history(history, past_points, frequency, now=None):
   return history
 
 
-def ping(server, method='ping', timeout=2, ping_ver=None):
-  """Ping "server", and return the ping time in milliseconds.
-  If the ping fails, returns 0.
-  If the method is "curl", the returned time is the "time_connect" variable of
-  curl's "-w" option (multiplied by 1000 to get ms). In practice the time is
-  very similar to a simple ping."""
-  devnull = open(os.devnull, 'w')
-  # Build command.
-  assert method in ['ping', 'curl'], 'Error: Invalid ping method'
-  if method == 'ping':
-    # Timeout depends on which version of ping. If it can't be determined, don't set timeout.
-    if ping_ver == 'iputils':
-      command = ['ping', '-n', '-c', '1', '-w', str(timeout), server]
-    elif ping_ver == 'bsd':
-      command = ['ping', '-n', '-c', '1', '-t', str(timeout), server]
-    else:
-      command = ['ping', '-n', '-c', '1', server]
-  elif method == 'curl':
-    command = ['curl', '-s', '--output', '/dev/null', '--write-out', r'%{time_connect}',
-               '--connect-timeout', str(timeout), server]
-  # Call command.
-  try:
-    output = subprocess.check_output(command, stderr=devnull)
-    exit_status = 0
-  except subprocess.CalledProcessError as cpe:
-    output = cpe.output
-    exit_status = cpe.returncode
-  except OSError:
-    output = ''
-    exit_status = 1
-  finally:
-    devnull.close()
-  # Parse output or return 0.0 on error.
-  if exit_status == 0:
-    if method == 'ping':
-      return parse_ping(output)
-    elif method == 'curl':
-      return parse_curl(output)
-  else:
-    return 0.0
-
-
-def parse_ping(ping_str):
-  """Parse out the ms of the ping from the output of `ping -n -c 1`"""
-  ping_pattern = r' bytes from .* time=([\d.]+) ?ms'
-  for line in ping_str.splitlines():
-    match = re.search(ping_pattern, line)
-    if match:
-      try:
-        return float(match.group(1))
-      except ValueError:
-        return 0.0
-  return 0.0
-
-
-def parse_curl(curl_str):
-  """Parse the output of curl into a number of milliseconds (float)."""
-  try:
-    return 1000*float(curl_str)
-  except ValueError:
-    return 0.0
-
-
-def ping_and_check(timeout=2, server='www.gstatic.com', path='/generate_204', status=204, body=''):
-  """"Ping" a server with an HTTP GET request, returning the latency and whether
-  the response appears to be intercepted (i.e. by a captive portal).
-  By default, uses http://www.gstatic.com/generate_204 and assumes interception
-  if the response code isn't 204 or the body isn't "". You can customize the
-  url and expected response with the respective parameters.
-  The latency is determined from the TCP handshake, so it should be a single
-  round trip.
-  Returns (float, bool): latency in milliseconds and whether the response looks
-  intercepted. If no connection can be established, returns (0.0, None). If an
-  error is encountered at any point, returns None for the second value."""
-  elapsed, response = ping_http(timeout=timeout, server=server, path=path, body=body)
-  if response is None:
-    return (0.0, None)
-  # Is the response as expected?
-  # If only an expected status is given (body is None), only that has to match.
-  # If a status and body is given, both have to match.
-  if response['status'] == status and (body is None or response['body'] == body):
-    expected = True
-  else:
-    expected = False
-  return (elapsed, not expected)
-
-
-def ping_http(timeout=2, server='www.gstatic.com', path='/generate_204', body=''):
-  # Do the DNS lookup outside the timed portion of the connection, where we only want to measure the
-  # TCP handshake, not any needed DNS lookup.
-  ip = dns_lookup(server, timeout=timeout)
-  # Create the connection object.
-  conex = httplib.HTTPConnection(ip, timeout=timeout)
-  # Open a connection to the server. connect() just establishes the TCP connection with a
-  # SYN, SYN/ACK, ACK handshake, returning after the final ACK is sent. This is essentially
-  # immediately after the SYN/ACK arrives, making it a good measure of a single round trip.
-  before = timeit.default_timer()
-  try:
-    conex.connect()
-  except (httplib.HTTPException, socket.error):
-    return (0.0, None)
-  after = timeit.default_timer()
-  elapsed = round(1000 * (after - before), 1)
-  # Make the HTTP request.
-  # We have to define the Host header explicitly to avoid it appearing as the IP address.
-  # Note: This won't work with HTTPS, since the host will be passed in the SNI.
-  headers = HTTP_HEADERS.copy()
-  headers['Host'] = server
-  try:
-    conex.request('GET', path, None, headers)
-  except (httplib.HTTPException, socket.error):
-    return (0.0, None)
-  # Read the HTTP response.
-  try:
-    response = conex.getresponse()
-  except (httplib.HTTPException, socket.error):
-    return (0.0, None)
-  # We have to pass back a dict of response values instead of the response itself because you can't
-  # read the response body after the connection is closed.
-  response_dict = {'status':response.status, 'body':response.read(len(body)+1)}
-  conex.close()
-  return (elapsed, response_dict)
-
-
-def dns_lookup(domain, timeout=2):
-  """Do a DNS lookup with a certain timeout, if possible."""
-  try:
-    # Try using dns.resolver, if it's installed.
-    return dns_lookup_dns(domain, timeout=timeout)
-  except NameError:
-    # Otherwise, fall back to socket.gethostbyname().
-    return dns_lookup_socket(domain)
-
-
-def dns_lookup_dns(domain, timeout=2):
-  """Use "dns" module to look up an IP address, returning in a specified amount of time.
-  Returns None on timeout or error."""
-  resolver = dns.resolver.Resolver()
-  resolver.timeout = timeout
-  resolver.lifetime = timeout
-  try:
-    results = resolver.query(domain)
-  except dns.exception.DNSException:
-    return None
-  try:
-    # Just pick the first IP address returned.
-    return str(results[0])
-  except IndexError:
-    return None
-
-
-def dns_lookup_socket(domain):
-  """Use socket.gethostbyname() to look up an IP address.
-  Returns None on error."""
-  #TODO: Set timeout using signal.alarm():
-  #      https://stackoverflow.com/questions/492519/timeout-on-a-python-function-call/494273#494273
-  try:
-    ip = socket.gethostbyname(domain)
-  except (socket.timeout, socket.gaierror):
-    return None
-  return ip
-
-
 def write_history(history_file, history):
   """Write the current history data structure to the history file.
   See get_history() for the format of the "history" data structure."""
@@ -638,39 +465,6 @@ def sleep(target, delay=5, precision=0.1):
     time.sleep(precision)
     now = int(time.time())
   return target + delay
-
-
-def get_ping_version():
-  """Try to determine what version of ping is available.
-  Currently recognizes the BSD and iputils versions, returning "bsd" and
-  "iputils" for them, respectively. For all others, returns the first line of
-  the output of "ping -V". On error, returns None."""
-  devnull = open(os.devnull, 'w')
-  # Call command.
-  try:
-    output = subprocess.check_output(['ping', '-V'], stderr=devnull)
-    exit_status = 0
-  except subprocess.CalledProcessError as cpe:
-    output = cpe.output
-    exit_status = cpe.returncode
-  except OSError:
-    output = ''
-    exit_status = 1
-  finally:
-    devnull.close()
-  # BSD lacks -V option and errors out with exit status 64
-  if exit_status == 64 and output == '':
-    return 'bsd'
-  elif exit_status != 0:
-    return None
-  # Parse output
-  output_lines = output.splitlines()
-  if len(output_lines) == 0:
-    return None
-  if 'iputils' in output_lines[0]:
-    return 'iputils'
-  else:
-    return output_lines[0]
 
 
 def describe_detectors(detectors):
