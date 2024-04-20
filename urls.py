b41bc0b6d39dfa7ce55336091f6fee78ce84beea
Nick <git@mailship.org>
2024-04-20 00:10:11 -0400

    urls.py: Update for Django 4.x compatibility.

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'uptest'
 
 urlpatterns = [
-  url(r'^polo$', views.reply, name='reply'),
+  re_path(r'^polo$', views.reply, name='reply'),
 ]
